cabfaaf4e87fda566a39533b3bfd9f36a70a74b6
Fikile1995 <62398518+Fikile1995@users.noreply.github.com>
2020-09-11 10:05:36 +0200

    Add files via upload

---
+++ Fikile Matayi/script.js
@@ -0,0 +1,25 @@
+// changes the size of the image when the mouse is over the image
+function bigImg(x){
+    x.style.width="400px"
+}
+
+function normalImg(x){
+    x.style.width="300px"
+}
+
+function changeColor1(){
+    var color=document.getElementById("navbar");
+    color.style.backgroundColor="red"
+}
+function changeColor2(){
+    var color=document.getElementById("navbar");
+    color.style.backgroundColor="pink"
+}
+function changeColor3(){
+    var color=document.getElementById("navbar");
+    color.style.backgroundColor="green"
+}
+function changeColor4(){
+    var color=document.getElementById("navbar");
+    color.style.backgroundColor="yellow"
+}
